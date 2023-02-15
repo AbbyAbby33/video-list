47f41ccef577010c400c673168df4d8ae33ee072
Abby <sk573129@gmail.com>
2023-02-16 01:01:03 +0800

    refactor: 切 tag 共用元件

---
+++ components/Tag/index.tsx
@@ -0,0 +1,26 @@
+import styles from './style.module.scss'
+
+type TagType = {
+    id: string,
+    flag: string,
+}
+
+export default function Tag(props: TagType) {
+    const { id, flag } = props;
+    const name = new Map([
+        ['captions-cht', '中文'],
+        ['captions-ja', '日文'],
+        ['captions-vi', '越南文'],
+        ['captions-en', '英文'],
+        ['level-0', '初級'],
+        ['level-1', '中級'],
+        ['level-2', '中高級'],
+        ['level-3', '高級']
+    ]);
+
+    return (
+        <span className={styles[`${flag}-${id}`]}>
+            {name.get(`${flag}-${id}`)}
+        </span>
+    )
+}
--- components/VideoList/index.tsx
+++ components/VideoList/index.tsx
@@ -1,19 +1,8 @@
+import Tag from '../Tag'
 import styles from './style.module.scss'
 
 export default function VideoList(props: any) { // TODO: 型別
     const { videoList } = props;
-    const level = {
-        1: '初級',
-        2: '中級',
-        3: '中高級',
-        4: '高級'
-    }
-    const captions = {
-        'cht': '中文',
-        'ja': '日文',
-        'vi': '越南文',
-        'en': '英文',
-    }
 
     return (
         <>
@@ -34,8 +23,12 @@ export default function VideoList(props: any) { // TODO: 型別
                                                 <span className={styles.views}>{v.views}</span>
                                             </span>
                                             <span className={styles['button-bar']}>
-                                                <span className={styles[`captions-${v.captions[0]}`]}>{captions[v.captions[0]]}</span> {/* TODO: 型別 */}
-                                                <span className={styles[`level-${v.level}`]}>{level[v.level]}</span> {/* TODO: 型別 */}
+                                                <span>
+                                                    <Tag flag='captions' id={v.captions[0]} />
+                                                </span>
+                                                <span>
+                                                    <Tag flag='level' id={v.level} />
+                                                </span>
                                             </span>
                                         </div>
                                     </div>
