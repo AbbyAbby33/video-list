ffe914ac1250d07c4524db944a3fecdc5b9c1687
Abby <sk573129@gmail.com>
2023-02-16 23:00:41 +0800

    feat: 新增影片時長

--- components/VideoItem/index.tsx
+++ components/VideoItem/index.tsx
@@ -1,16 +1,43 @@
 import VideoItemInterface from '@/models/VideoItemInterface';
+import { useEffect, useRef, useState } from 'react'
 import Tag from '../Tag'
 import styles from './style.module.scss'
 
 export default function VideoItem(props: VideoItemInterface) {
-    const { id, thumbnail, title, views, captions, level } = props;
+    const { id, thumbnail, title, views, captions, level, duration } = props;
     // console.log('props', props)
+
+    const isFirstRun = useRef(true);
+    const [durationString, setDurationString] = useState('');
+
+    useEffect(() => {
+        if (isFirstRun.current) {
+            isFirstRun.current = false;
+        } else {
+            // console.log(duration);
+            const d = countDuration(duration);
+            setDurationString(d);
+        }
+    }, [duration]);
+
+    const countDuration = (duration: number) => {
+        // TODO: 小時
+        let r, m, s;
+        s = duration % 60;
+        m = ((duration - s) / 60).toString().padStart(2, '0');
+        s = s.toString().padStart(2, '0');
+        r = `${m}:${s}`
+        return r;
+    }
 
     return (
         <div key={id} className={styles['video-item-wrap']}>
             <div className={styles['video-item']}>
                 {/* 影片縮圖 */}
-                <div className={styles['video-pic']} style={{ backgroundImage: `url(${thumbnail})` }}></div>
+                <div className={styles['video-pic-wrap']}>
+                    <div className={styles['video-pic']} style={{ backgroundImage: `url(${thumbnail})` }}></div>
+                    <span className={styles['video-duration']}>{durationString}</span>
+                </div>
                 {/* 影片資訊 */}
                 <div className={styles['video-info']}>
                     <p className={styles.title}>{title}</p>
